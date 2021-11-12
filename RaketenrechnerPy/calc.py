33fe549a7fe9806ec5798edb5bbf8afb24952383
Janik-ux <mianikproductions@gmail.com>
2021-11-12 22:24:35 +0100

    forgot square root at Vwater line 26

--- RaketenrechnerPy/calc.py
+++ RaketenrechnerPy/calc.py
@@ -15,7 +15,7 @@ step = 0.1 # wie genau auflösen? 1 sekunde # TODO ist wahrscheinlich zu ungenau
 # daten für iterative vorgehensweise
 # vars to be changed every iteration
 masse       = startmasse # gramm
-second      = 0
+second      = step
 pressure    = initialpressure
 Vrakete     = 0 # m/s
 
@@ -23,15 +23,16 @@ Vrakete     = 0 # m/s
 while second <= breakSecond:
     print("----------Sekunde: {}----------".format(second))
     # iterieren pro sekunde
-    Vwasser = (2*pressure/dichte) # m/s keine Ahnung, warum man den Durchmesser nicht braucht 
+    Vwasser = (2*pressure/dichte)**0.5 # m/s keine Ahnung, warum man den Durchmesser nicht braucht 
                                   # siehe auch https://www.physikerboard.de/topic,45753,-wasserstrahlaustrittsgeschwindigkeit-einer-1mm-duese.html
     austrMasse = duesenflaeche * Vwasser * step * 1000 # kg weil m^2 * m/s * 1s = m^3, m^3*kg/m^3=kg
     thrust = austrMasse*Vwasser # kg*m/s = N
     twr = thrust/(masse*constants.g)
     Vrakete += thrust/masse*step # impulsänderung durch Wasseraustritt (+m/s)
     Vrakete -= constants.g*step # impulsänderung durch Gravitation (-m/s)
-    print("Vrakete: {} m/s,\nTWR: {},\naustrMasse: {} kg,\nmasse: {} kg\n".format(round(Vrakete, 2), round(twr, 2), round(austrMasse, 2), round(masse, 2)))
+    print("Vrakete: {} m/s,\nVwasser: {} m/s,\nTWR: {},\naustrMasse: {} kg,\nmasse: {} kg\n".format(round(Vrakete, 2), round(Vwasser, 2), round(twr, 2), round(austrMasse, 2), round(masse, 2)))
     masse -= austrMasse
     if masse <= 0:
         break # quickfix normal mit druck regeln, sodass einfach keine austrMasse mehrabgezogen wird.
     second += step
+    second = round(second, 1)
