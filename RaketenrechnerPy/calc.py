f9d9912a374e9d305fb9e740da21f3bf65dfa4de
Janik-ux <mianikprdouctions@gmail.com>
2021-11-15 22:33:29 +0100

    added pressure to sigma loop

--- RaketenrechnerPy/calc.py
+++ RaketenrechnerPy/calc.py
@@ -4,14 +4,21 @@ import matplotlib.pyplot as plt
 # ----inital data----
 # not to be changed from code itself
 # here configure youir rocket data
-startmasse  = 1.745 # kg
-leermasse   = 0.445 # kg
 dichte      = 1000 # kg/m^3
+wasserfuellanteil = 1/3
+luftfuellanteil = 1-wasserfuellanteil
+leermasse   = 0.445 # kg
+gesamtvolumen = 4.6 * 10**-3 # L * 10^-3 = m^3
+wassermasse = gesamtvolumen*wasserfuellanteil*dichte
+startmasse = wassermasse + leermasse
+initialLuftvolumen = gesamtvolumen*luftfuellanteil
 initialpressure = 15 * 10**5 # Pa
 breakSecond = 5
 duesendurchmesser = 0.022 # m
 duesenflaeche     = math.pi * (duesendurchmesser/2)**2 # m^2 unter Annahme einer runden Düse
 step = 0.1 # wie genau auflösen? 1 sekunde # TODO ist wahrscheinlich zu ungenau
+
+dummy = 1234.234
 
 # matplotlib
 x_list = []
@@ -24,29 +31,33 @@ masse       = startmasse # gramm
 second      = step
 pressure    = initialpressure
 Vrakete     = 0 # m/s
+luftvolumen = initialLuftvolumen
 
 # powered ascend
+# loop for sigma function
 while second <= breakSecond:
     print("----------Sekunde: {}----------".format(second))
     # iterieren pro sekunde
 
     # berechnen:
-    Vwasser = (2*pressure/dichte)**0.5 # m/s keine Ahnung, warum man den Durchmesser nicht braucht 
-                                  # siehe auch https://www.physikerb    oard.de/topic,45753,-wasserstrahlaustrittsgeschwindigkeit-einer-1mm-duese.html
-    austrMasse = duesenflaeche * Vwasser * step * dichte # kg weil m^2 * m/s * 1s = m^3, m^3*kg/m^3=kg
-    thrust = austrMasse*Vwasser # kg*m/s = N
-    twr = thrust/(masse*constants.g)
+    Vwasser        = (2*pressure/dichte)**0.5 # m/s keine Ahnung, warum man den Durchmesser nicht braucht, siehe auch https://www.physikerboard.de/topic,45753,-wasserstrahlaustrittsgeschwindigkeit-einer-1mm-duese.html
+    austrVolumen   = duesenflaeche * Vwasser * step # m^2 * m/s * s = m^3
+    austrMasse     = austrVolumen * dichte           # m^3*kg/m^3=kg
+    thrust         = austrMasse*Vwasser # kg*m/s = N
+    twr            = thrust/(masse*constants.g)
     beschleunigung = thrust/masse*step - constants.g*step # impulsänderung durch Wasseraustritt (+m/s) - impulsänderung durch Gravitation (-m/s)
-    Vrakete += beschleunigung
+    Vrakete       += beschleunigung
 
-    # auswerten:
-    print("Vrakete: {} m/s,\nVwasser: {} m/s,\nbeschleunigung: {} m/s^2,\nTWR: {},\naustrMasse: {} kg,\nmasse: {} kg\n".format(round(Vrakete, 2), round(Vwasser, 2), round(beschleunigung, 2), round(twr, 2), round(austrMasse, 2), round(masse, 2)))
+    # auswerten:                                                                                                                                                              Vrakete            Vwasser            beschleunigung            twr            austrMasse            austrVolumen            masse            pressure
+    print("Vrakete: {} m/s,\nVwasser: {} m/s,\nbeschleunigung: {} m/s^2,\nTWR: {},\naustrMasse: {} kg,\naustrVolumen: {} m^3,\nmasse: {} kg,\npressure: {} bar\n".format(round(Vrakete, 2), round(Vwasser, 2), round(beschleunigung, 2), round(twr, 2), round(austrMasse, 2), round(austrVolumen, 5), round(masse, 2), round(pressure/10**5, 2)))
     x_list.append(second)
     twr_list.append(twr)
     Vrakete_list.append(Vrakete)
 
     # vars für nächsten step bereit machen
-    masse -= austrMasse
+    masse       -= austrMasse
+    luftvolumen += austrVolumen # die Luft hat nun etwas mehr platz, da Wasser fehlt
+    pressure    = initialpressure*initialLuftvolumen/luftvolumen # Pa*m^3/m^3=Pa
     # if masse <= leermasse:
     #     break # quickfix normal mit druck regeln, sodass einfach keine austrMasse mehrabgezogen wird.
     second += step
